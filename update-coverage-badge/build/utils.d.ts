f915ac342683448e8db955a2671e3c636805fd6f
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2021-01-26 19:26:00 +0200

    added simple-git and option to push within action

---
+++ update-coverage-badge/build/utils.d.ts
@@ -0,0 +1,2 @@
+declare const replacer: (pathToJsonSummary: string, pathToReadme: string) => Promise<void>;
+export { replacer };
---
+++ update-coverage-badge/build/utils.js
@@ -0,0 +1,50 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.replacer = void 0;
+const fs_1 = __importDefault(require("fs"));
+const simple_git_1 = __importDefault(require("simple-git"));
+const git = simple_git_1.default();
+const DEFAULT_README_PATH = './README.md';
+const DEFAULT_JSON_SUMMARY_PATH = './coverage/coverage-summary.json';
+const BADGE_REGEX = /Coverage%20(.+)\-([.0-9]+)%25-(.+)\.svg/g;
+const replacer = (pathToJsonSummary, pathToReadme) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const readmePath = pathToReadme || DEFAULT_README_PATH;
+        const summary = fs_1.default.readFileSync(pathToJsonSummary || DEFAULT_JSON_SUMMARY_PATH, 'utf-8');
+        const { total } = JSON.parse(summary);
+        const readMe = fs_1.default.readFileSync(readmePath, 'utf-8');
+        const updatedReadme = updateReadme(total, readMe);
+        fs_1.default.writeFileSync(pathToReadme, updatedReadme, 'utf-8');
+        console.log(yield git.add(pathToReadme));
+        console.log(yield git.commit('Updated file with badges'));
+        console.log(yield git.push());
+    }
+    catch (e) {
+        throw new Error(e);
+    }
+});
+exports.replacer = replacer;
+// if no code coverage badges were found, append new badges to readme file
+const updateReadme = (total, readMe) => !BADGE_REGEX.test(readMe) ?
+    prependNewBadges(total, readMe) :
+    replaceOldBadges(total, readMe);
+const replaceOldBadges = (total, readMe) => readMe.replace(BADGE_REGEX, (match, name) => getCoverageBadge(name, total));
+const getCoverageBadge = (name, total) => allCoverage(name, total);
+const getBadge = (value, percentage) => `Coverage%20${value}-${percentage}%25-${getBadgeColor(percentage)}.svg`;
+const getBadgeColor = (percentage) => percentage < 70 ? 'red' : (percentage < 80 ? 'orange' : 'green');
+const allCoverage = (name, total) => getBadge(name, total[name.toLowerCase()].pct);
+const badgeTemplate = (value, percentage) => `![Coverage badge](https://img.shields.io/badge/${getBadge(value, percentage)})`;
+const createBadges = (total) => Object.keys(total).map(key => badgeTemplate(key, total[key.toLowerCase()].pct)).join('\n');
+const prependNewBadges = (total, pathToReadme) => `${createBadges(total)}\n${fs_1.default.readFileSync(pathToReadme)}`;
